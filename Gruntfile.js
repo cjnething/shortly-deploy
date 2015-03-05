38c4153321a4b0ac1449e23938a703421fc0a6eb
Hack Reactor Students <accounts@hackreactor.com>
2015-03-05 11:14:47 -0800

    (almost) finish Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,34 +4,34 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
 
-     concat: {
+    concat: {
 
       options: {
         separator: ';'
       },
 
 
-        app: {
-          src: ['public/client/app.js',
-                'public/client/link.js',
-                'public/client/links.js',
-                'public/client/linkView.js',
-                'public/client/linksView.js',
-                'public/client/createLinkView.js',
-                'public/client/router.js'
-          ],
-          dest: 'public/dist/application.js'
-        },
-
-        lib: {
-          src: ['public/lib/jquery.js',
-                'public/lib/underscore.js',
-                'public/lib/backbone.js',
-                'public/lib/handlebars.js'
-              ],
-          dest: 'public/dist/libraries.js'
-        },
+      app: {
+        src: ['public/client/app.js',
+        'public/client/link.js',
+        'public/client/links.js',
+        'public/client/linkView.js',
+        'public/client/linksView.js',
+        'public/client/createLinkView.js',
+        'public/client/router.js'
+        ],
+        dest: 'public/dist/application.js'
       },
+
+      lib: {
+        src: ['public/lib/jquery.js',
+        'public/lib/underscore.js',
+        'public/lib/backbone.js',
+        'public/lib/handlebars.js'
+        ],
+        dest: 'public/dist/libraries.js'
+      },
+    },
 
     mochaTest: {
       test: {
@@ -59,92 +59,122 @@ module.exports = function(grunt) {
         }
       },
 
-    jshint: {
-      files: [
-        // Add filespec list here
-      ],
-      options: {
-        force: 'true',
-        jshintrc: '.jshintrc',
-        ignores: [
+      jshint: {
+        files: [
+        'Gruntfile.js'
+        ],
+        options: {
+          force: 'true',
+          jshintrc: '.jshintrc',
+          ignores: [
           'public/lib/**/*.js',
           'public/dist/**/*.js'
-        ]
-      }
-    },
+          ]
+        }
+      },
 
-    cssmin: {
-    },
+      cssmin: {
+        target: {
+          files: [{
+            expand: true,
+            cwd: 'public/',
+            src: ['style.css'],
+            dest: 'public/dist',
+            ext: '.min.css'
+          }]
+        }
+      },
 
-    watch: {
-      scripts: {
-        files: [
+      watch: {
+        scripts: {
+          files: [
           'public/client/**/*.js',
           'public/lib/**/*.js',
-        ],
-        tasks: [
+          ],
+          tasks: [
           'concat',
           'uglify'
-        ]
+          ]
+        },
+        css: {
+          files: 'public/*.css',
+          tasks: ['cssmin']
+        }
       },
-      css: {
-        files: 'public/*.css',
-        tasks: ['cssmin']
-      }
-    },
 
-    shell: {
-      prodServer: {
-      }
-    },
-  });
+      shell: {
+        prodServer: {
+          command: 'git push azure bugfix'
+        }
+      },
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-mocha-test');
+grunt.loadNpmTasks('grunt-shell');
+grunt.loadNpmTasks('grunt-nodemon');
+grunt.loadNpmTasks('grunt-git-deploy');
 
-  grunt.registerTask('server-dev', function (target) {
+grunt.registerTask('server-dev', function (target) {
     // Running nodejs in a different process and displaying output on the main console
     var nodemon = grunt.util.spawn({
-         cmd: 'grunt',
-         grunt: true,
-         args: 'nodemon'
-    });
+     cmd: 'grunt',
+     grunt: true,
+     args: 'nodemon'
+   });
     nodemon.stdout.pipe(process.stdout);
     nodemon.stderr.pipe(process.stderr);
 
     grunt.task.run([ 'watch' ]);
   });
 
+
+grunt.registerTask('server', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var git = grunt.util.spawn({
+     cmd: 'git',
+     grunt: true,
+     args: 'add'
+   });
+    git.stdout.pipe(process.stdout);
+    git.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
   ////////////////////////////////////////////////////
   // Main grunt tasks
   ////////////////////////////////////////////////////
 
   grunt.registerTask('test', [
+    'jshint',
     'mochaTest'
-  ]);
+    ]);
 
   grunt.registerTask('build', [
-  ]);
+    'concat:app',
+    'concat:lib',
+    'uglify',
+    'cssmin'
+    ]);
 
-  grunt.registerTask('default', ['concat:app', 'concat:lib', 'uglify']);
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
       // add your production server task here
+      grunt.task.run([ 'shell' ]);
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
   grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
-
+    'test',
+    'build',
+    'upload'
+    ]);
 
 };
