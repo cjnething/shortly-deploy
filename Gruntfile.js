34582838785895300185b766ee76f14bcc237990
Hack Reactor Students <accounts@hackreactor.com>
2015-03-04 19:21:22 -0800

    broken

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,6 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-    },
 
     mochaTest: {
       test: {
@@ -14,6 +12,35 @@ module.exports = function(grunt) {
       }
     },
 
+    copy: {
+      build: {
+        cwd: 'source',
+        src: [ '**' ],
+        dest: 'build',
+        expand: true
+      },
+    },
+
+    clean: {
+      build: {
+        src: [ 'build' ]
+      },
+    },
+
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['public/client/*.js' ],
+        // the location of the resulting JS file
+        //'dist/<%= pkg.name %>.js'
+        dest: 'dist/application.js'
+      }
+   },
+
     nodemon: {
       dev: {
         script: 'server.js'
@@ -21,11 +48,37 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      static_mappings: {
+          // Because these src-dest file mappings are manually specified, every
+          // time a new file is added or removed, the Gruntfile has to be updated.
+          files: [
+          {src: 'dist/application.js', dest: 'dist/application.min.js'}
+          ],
+        }
+
+     //  dynamic_mappings: {
+     //  // Grunt will search for "**/*.js" under "lib/" when the "uglify" task
+     //  // runs and build the appropriate src-dest file mappings then, so you
+     //  // don't need to update the Gruntfile when files are added or removed.
+     //    files: [
+     //      {
+     //        expand: true,     // Enable dynamic expansion.
+     //        cwd: 'public/',      // Src matches are relative to this path.
+     //        src: ['**/*.js'], // Actual pattern(s) to match.
+     //        dest: 'build/',   // Destination path prefix.
+     //        ext: '.min.js',   // Dest filepaths will have this extension.
+     //        extDot: 'first'   // Extensions in filenames begin after the first dot
+     //      }
+     //    ],
+     // }
     },
 
     jshint: {
       files: [
-        // Add filespec list here
+        'Gruntfile.js',
+        'test/**/*.js',
+        'server.js',
+        'public/client/**/*.js'
       ],
       options: {
         force: 'true',
@@ -38,7 +91,16 @@ module.exports = function(grunt) {
     },
 
     cssmin: {
-    },
+        target: {
+          files: [{
+            expand: true,
+            cwd: 'public/',
+            src: ['*.css', '!*.min.css'],
+            dest: 'build/',
+            ext: '.min.css'
+          }]
+        }
+     },
 
     watch: {
       scripts: {
@@ -68,6 +130,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
@@ -90,15 +154,20 @@ module.exports = function(grunt) {
   ////////////////////////////////////////////////////
 
   grunt.registerTask('test', [
+    'jshint',
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
+   grunt.registerTask('default', ['test', 'concat', 'uglify', 'cssmin']);
+
+  grunt.registerTask('build', ['clean', 'copy'
   ]);
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
       // add your production server task here
+
+
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
@@ -107,6 +176,4 @@ module.exports = function(grunt) {
   grunt.registerTask('deploy', [
     // add your deploy tasks here
   ]);
-
-
 };
--- server-config.js
+++ server-config.js
@@ -11,7 +11,7 @@ app.configure(function() {
   app.set('view engine', 'ejs');
   app.use(partials());
   app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/dist'));
   app.use(express.cookieParser('shhhh, very secret'));
   app.use(express.session());
 });
